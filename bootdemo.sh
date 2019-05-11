2872a9f0cd2d744c61b02df2190a415c96622958
linmingwei <linmingwei100@gmail.com>
2019-05-11 23:14:28 +0800

    begin deploy centos

---
+++ bootdemo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 编译bootdemo
+echo "------------------------------build bootdemo begin---------------------------------"
+mvn clean install -DskipTests;
+# 拷贝demo.jar 到centos
+echo "------------------------------copy bootdemo.jar begin---------------------------------"
+scp target/bootdemo.jar mwl@192.168.41.133:~/github;
+# 更新数据库
+echo "------------------------------update bootdemo database begin---------------------------------"
+scp boot.sql mwl@192.168.41.133:~/github;
+mysql -h 192.168.41.133 -uroot -pmysql123 bootdemo < boot.sql;
