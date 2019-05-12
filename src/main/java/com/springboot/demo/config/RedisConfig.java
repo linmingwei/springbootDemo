71708f4c8214c1657312c899c29955f169ea8a32
linmingwei <linmingwei100@gmail.com>
2019-05-12 23:28:43 +0800

    begin redis

---
+++ src/main/java/com/springboot/demo/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.springboot.demo.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @author ldd
+ * @desc
+ * @create 2019-05-12 16:15
+ **/
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String ,Serializable> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String ,Serializable> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(factory);
+        return template;
+    }
+}
